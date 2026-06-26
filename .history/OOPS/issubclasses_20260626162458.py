8c9992b8d44dd42147261ee4400923aa25ada7bf
dgtldeveloper <developer@dgtlinnovtions.in>
2026-06-26 17:32:15 +0530

    c

---
+++ .history/OOPS/issubclasses_20260626162458.py
@@ -0,0 +1,34 @@
+class Parent(object):
+    pass
+
+class Child(Parent):
+    pass
+
+print(issubclass(Parent, Child))
+print(issubclass(Child, Parent))
+
+obj1 = Child()
+obj2 = Parent()
+
+print(isinstance(obj2, Parent))
+print(isinstance(obj2, Child))
+
+st = "abdc1321"
+
+res = st.isalnum()
+print(res)
+
+import re
+
+print(bool(re.match('[A-Za-z0-9]+$','abd&@c1321')))
+
+
+# Encapsulation 
+
+class Person:
+    def __init__(self, __age, name):
+        self.__age = __age
+        self.name = name
+
+    def details(self):
+        print("Details of person", self.name)    
